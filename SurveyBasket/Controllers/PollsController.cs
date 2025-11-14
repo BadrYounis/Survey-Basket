763dc9a47b1fcf31e292d9df61faee85c0e4ef15
Badr Younis <badryounis14@gmail.com>
2025-11-14 23:21:34 +0200

    Add CRUD operations to Polls Controller

---
+++ SurveyBasket/Controllers/PollsController.cs
@@ -0,0 +1,50 @@
+﻿namespace SurveyBasket.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PollsController(IPollService _pollService) : ControllerBase
+{
+    [HttpGet("")]
+    public IActionResult GetAll()
+    {
+        return Ok(_pollService.GetAll());
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult Get(int id)
+    {
+        var poll = _pollService.Get(id);
+
+        return poll is null ? NotFound() : Ok(poll);
+    }
+
+    [HttpPost("")]
+    public IActionResult Add(Poll request)
+    {
+        var newPoll = _pollService.Add(request);
+
+        return CreatedAtAction(nameof(Get), new {id = newPoll.Id}, newPoll);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult Update(int id, Poll request)
+    {
+       var isUpdated = _pollService.Update(id, request);
+
+       if(!isUpdated)
+            return NotFound();
+
+        return NoContent();  
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        var isDeleted = _pollService.Delete(id);
+
+        if (!isDeleted)
+            return NotFound();
+
+        return NoContent();
+    }
+}
